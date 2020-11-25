8e30f322908a76e150e3f8e929235abce20cf343
rogo6 <piotrrogozinski95@gmail.com>
2020-11-25 20:08:00 +0100

    Solved dependencies version problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.styles.scss";
 
-import LoginPage from "./pages/login/login.component";
-import appLogo from "./assets/appLogo.png";
-
 function App() {
-  return (
-    <div>
-      <div className={"logoContainer"}>
-        <img src={appLogo} alt={"appLogo"} />
-      </div>
-      <Router>
-        <Switch>
-          <Route exact path={"/"} component={LoginPage} />
-        </Switch>
-      </Router>
-    </div>
-  );
+  return <div>Hello</div>;
 }
 
 export default App;
--- src/pages/homepage/homepage.component.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./homepage.styles.scss";
-
-const Homepage = () => {
-  return <div className={"homepage"}>Homepage welcome!</div>;
-};
-
-export default Homepage;
